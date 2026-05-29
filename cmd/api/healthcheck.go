b0aaba70b0d1e25146be9dd4b3d48ceb1c4801e4
amh1k <abdulmoizx97@gmail.com>
2026-05-29 22:50:46 +0500

    refactor(api): extract server configuration and graceful shutdown functionality

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -16,6 +16,7 @@ func(app *application)healthcheckHandler(w http.ResponseWriter, r *http.Request)
             "version":     version,
         },
     }
+  
 	err := app.writeJSON(w, http.StatusOK, env, nil)
 	if err != nil {
        app.serverErrorResponse(w, r, err)
